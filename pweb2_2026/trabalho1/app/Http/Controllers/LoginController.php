5bc2fc13ebaa1be6f96a0ae4e493ad92742b9b90
FeguinhaKill <feguinhak2@gmail.com>
2026-03-11 16:56:02 -0300

    Login controler arrumado

--- pweb2_2026/trabalho1/app/Http/Controllers/LoginController.php
+++ pweb2_2026/trabalho1/app/Http/Controllers/LoginController.php
@@ -13,5 +13,47 @@ class LoginController extends Controller
         $dados = Login::all();
         return view('login', ['dados' => $dados]);
     }
-    
+public function store(Request $request)
+{
+    $request->validate([
+        'nome' => 'required',
+        'email' => 'required|email',
+        'senha' => 'required|min:6',
+    ], [
+        'nome.required' => 'O nome é obrigatório',
+        'email.required' => 'O email é obrigatório',
+        'senha.required' => 'A senha é obrigatória',
+    ]);
+
+    Login::create([
+        'name' => $request->nome,
+        'email' => $request->email,
+        'password' =>$request->senha,,
+    ]);
+
+    return redirect('/login');
+}
+function update(Request $request, $id)
+{
+    $request->validate([
+        'nome' => 'required',
+        'email' => 'required|email',
+    ], [
+        'nome.required' => "O nome é obrigatório",
+        'email.required' => "O email é obrigatório",
+    ]);
+
+    $dados = [
+        'name' => $request->nome,
+        'email' => $request->email
+    ];
+
+    if ($request->filled('senha')) {
+        $dados['password'] = $request->senha;
+    }
+
+    Login::find($id)->update($dados);
+
+    return redirect('usuario');
+}    
 }
