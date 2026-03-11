2003a76c29b32c9ad5a568798d8c45844686d541
FeguinhaKill <feguinhak2@gmail.com>
2026-03-11 16:44:34 -0300

    servicos

---
+++ pweb2_2026/trabalho1/database/factories/ServicosFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Servicos;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Servicos>
+ */
+class ServicosFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
--- pweb2_2026/trabalho1/resources/views/Login/login.blade.php
+++ pweb2_2026/trabalho1/resources/views/Login/login.blade.php
@@ -1,15 +1,54 @@
-<form action="/login" method="post">
-  @csrf
-  <div class="form-group">
-    <label for="exampleInputEmail1">Nome </label>
-    <input type="nome" class="form-control" id="nome" placeholder="Nome..."></div>
-  <div class="form-group">
-    <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="email@gmail.com">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="senha" placeholder="senha...">
-  </div>
-  <button type="submit" class="btn btn-primary">Enviar</button>
+@extends('main')
+@section('titulo', 'Login')
+@section('conteudo')
+
+<h4>Login</h4>
+
+@php
+    $action = url('/login');
+@endphp
+
+<form action="{{ $action }}" method="POST">
+    @csrf
+
+    <div class="row">
+        <div class="col">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" 
+                   class="form-control" 
+                   name="nome" 
+                   value="{{ old('nome') }}" 
+                   placeholder="Nome...">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" 
+                   class="form-control" 
+                   name="email" 
+                   value="{{ old('email') }}" 
+                   placeholder="email@gmail.com">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <label for="senha" class="form-label">Senha</label>
+            <input type="password" 
+                   class="form-control" 
+                   name="senha" 
+                   placeholder="Senha...">
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col">
+            <button type="submit" class="btn btn-success">Enviar</button>
+        </div>
+    </div>
+
 </form>
+
+@stop
