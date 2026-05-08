fa5e1afc42038a7118fa5781ab3b20cda3073ef3
FeguinhaKill <feguinhak2@gmail.com>
2026-05-07 22:08:22 -0300

    criado 2 servicos manualmente

--- pweb2_2026/Felipe_S_Arthur_S/database/seeders/ServicosSeeder.php
+++ pweb2_2026/Felipe_S_Arthur_S/database/seeders/ServicosSeeder.php
@@ -12,6 +12,16 @@ class ServicosSeeder extends Seeder
      */
     public function run(): void
     {
-        Servicos::factory()->count(10)->create();
+        Servicos::factory()->count(8)->create();
+        Servicos::make([
+            'nome' => 'Instalação de Mira Prismática',
+            'preco' => '50.00',
+            'descricao' => 'Instalação de mira prismática em armas de fogo, garantindo precisão e estabilidade para atiradores.',
+        ])->save();
+        Servicos::make([
+            'nome' => 'Instalação de Mira Holográfica',
+            'preco' => '40.00',
+            'descricao' => 'Instalação de mira holográfica nas pistolas e submetralhadoras',
+        ])->save();
     }
 }
